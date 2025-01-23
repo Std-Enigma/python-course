36e4a43fca3d7009fc80ca9242934dc37a69bf10
Std-Enigma <std.ilia.bahrebar@gmail.com>
2025-01-23 10:41:02 +0330

    Add using library files

---
+++ Using_Libraries/main.py
@@ -0,0 +1,20 @@
+# Session 20 - Using Standard Libraries
+
+import math
+
+# 1. Using math library
+radius = 5
+area = math.pi * (radius**2)
+print("Area of the circle:", area)
+
+# 2. Random number generation
+import random
+
+random_number = random.randint(1, 100)
+print("Random number between 1 and 100:", random_number)
+
+# 3. Date and time
+import datetime
+
+current_time = datetime.datetime.now()
+print("Current date and time:", current_time)
