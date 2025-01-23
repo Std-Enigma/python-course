b7d347b1891cdac52b5443c81d3be7a0c5f7ad6c
Std-Enigma <std.ilia.bahrebar@gmail.com>
2025-01-23 10:39:50 +0330

    Add modules files

---
+++ Modules/main.py
@@ -0,0 +1,8 @@
+# Session 19 - Creating and Importing Modules
+import math_operations
+
+result_add = math_operations.add(5, 3)
+result_subtract = math_operations.subtract(5, 3)
+
+print("Addition Result:", result_add)  # 8
+print("Subtraction Result:", result_subtract)  # 2
